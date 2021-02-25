6de548aab2d14a7e98e8d4cfe908cb67a104d69d
demonkiller <hiranyagarbh7@gmail.com>
2021-02-25 17:55:09 +0530

    added allclean script

---
+++ allclean.sh
@@ -0,0 +1,24 @@
+echo "Removing all workfiles from Directory...."
+echo "Ignoring WorkFlow Files ......"
+echo "                                         "
+
+rm -rvf airoofs/
+echo "                                         "
+echo "removed airootfs, now removing efiboot.. "
+rm -rvf efiboot/
+echo "                                         "
+echo "removed efiboot, now removing syslinux..."
+rm -rvf syslinux/
+echo "                                         "
+echo "removed syslinux, now removing work and out"
+./cleanup.sh
+echo "                                         "
+echo "Now removing all remaining files:"
+rm -vf packages.x86_64
+rm -vf pacman.conf
+rm -vf profiledef.sh
+echo "                                         "
+echo "Removed all files and folders except workflow files."
+echo "============"
+echo "= D O N E! ="
+echo "============"
